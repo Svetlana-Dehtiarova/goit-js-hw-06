f6846acaf2461e6781d955604d91c81bc92504f2
Svetlana-Dehtiarova <degsvet1977@gmail.com>
2022-12-14 21:00:41 +0200

    Update tasks

---
+++ js/task-02.js
@@ -0,0 +1,24 @@
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+const ingredients = [
+  'Potatoes',
+  'Mushrooms',
+  'Garlic',
+  'Tomatos',
+  'Herbs',
+  'Condiments',
+];
+const ingredientsEl = document.querySelector("#ingredients");
+
+const createItemEl = ingredients.map( el => {
+  const productEl = document.createElement(`li`);
+  productEl.classList.add(`item`);
+  productEl.textContent = el;
+  console.log(productEl);
+  return productEl;
+});
+ingredientsEl.append(...createItemEl);
